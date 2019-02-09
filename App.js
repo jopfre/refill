d5443b14a25e993d87ed91922daaaf941e9ff439
jopfre <jonah.freeland@gmail.com>
2019-02-09 17:03:40 +0000

    add redux

--- App.js
+++ App.js
@@ -1,42 +1,21 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// import map from './components/map';
-import MapView, { Marker } from 'react-native-maps';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux';
 
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Text</Text>
-//       </View>
-//     );
-//   }
-// }
+import reducer from './reducer';
+
+import TheMap from './components/Map';
+
+const store = createStore(reducer);
 
 export default class App extends Component {
   render() {
     return (
-      <MapView
-        style={{flex: 1}}
-        region={{
-          latitude: 51.4545,
-          longitude: -2.5879,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-        showsUserLocation={true}>
-        <Marker coordinate={{latitude: 51.4545, longitude: -2.5879}}/>
-      </MapView>
+      <Provider store={store}>
+        <TheMap/>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
