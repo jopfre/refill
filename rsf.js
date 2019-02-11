4eb7e844bcf6f6a23971a520bd3fd1a65066773c
jopfre <jonah.freeland@gmail.com>
2019-02-11 17:13:59 +0000

    remove creds

--- rsf.js
+++ rsf.js
@@ -1,14 +1,9 @@
 import firebase from 'firebase'
 import ReduxSagaFirebase from 'redux-saga-firebase'
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAy-WvPAHUVZUU9ZZyVy6StLvoMXuPfDSc",
-  authDomain: "refill-free.firebaseapp.com",
-  databaseURL: "https://refill-free.firebaseio.com",
-  projectId: "refill-free",
-  storageBucket: "refill-free.appspot.com",
-  messagingSenderId: "173844871125"
-})
+import { creds } from './creds'
+console.log(creds);
+const firebaseApp = firebase.initializeApp(creds)
 
 const rsf = new ReduxSagaFirebase(firebaseApp)
 
